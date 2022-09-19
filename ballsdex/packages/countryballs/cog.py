8521d03d7d92f741dc517d205d05661947d2ac2b
retke <laggron42@gmail.com>
2022-09-19 18:56:20 +0200

    countryballs: spawnball and spawnstats admin commands

--- ballsdex/packages/countryballs/cog.py
+++ ballsdex/packages/countryballs/cog.py
@@ -4,9 +4,11 @@ import logging
 from typing import Optional
 from tortoise.exceptions import DoesNotExist
 from discord.ext import commands
+from discord.utils import format_dt
 
-from ballsdex.core.models import GuildConfig
+from ballsdex.core.models import GuildConfig, Ball
 from ballsdex.packages.countryballs.spawn import SpawnManager
+from ballsdex.packages.countryballs.countryball import CountryBall
 
 log = logging.getLogger("ballsdex.packages.countryballs")
 
@@ -64,3 +66,43 @@ class CountryBallsSpawner(commands.Cog):
                 del self.cache[guild.id]
             elif channel:
                 self.cache[guild.id] = channel.id
+
+    @commands.command()
+    @commands.is_owner()
+    async def spawnball(
+        self,
+        ctx: commands.Context,
+        channel: discord.TextChannel | None = None,
+        *,
+        ball: str | None = None,
+    ):
+        """
+        Force spawn a countryball.
+        """
+        if not ball:
+            countryball = await CountryBall.get_random()
+        else:
+            try:
+                ball_model = await Ball.get(country__iexact=ball.lower())
+            except DoesNotExist:
+                await ctx.send("No such ball exists.")
+                return
+            countryball = CountryBall(ball_model)
+        await countryball.spawn(channel or ctx.channel)
+
+    @commands.command()
+    @commands.is_owner()
+    @commands.guild_only()
+    async def spawnstats(self, ctx: commands.Context):
+        """
+        Get details about countryball spawner.
+        """
+        assert ctx.guild
+        if ctx.guild.id not in self.cache:
+            await ctx.send("That guild does not have a registered spawn channel.")
+            return
+        cooldown_manager = self.spawn_manager.cooldowns[ctx.guild.id]
+        await ctx.send(
+            f"Initiated {format_dt(cooldown_manager.time, style='R')}\n"
+            f"Counter: {cooldown_manager.amount}/{cooldown_manager.chance}"
+        )
