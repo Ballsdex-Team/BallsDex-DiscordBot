39e4fb828c01b27ca750423a8191586011d5f812
retke <laggron42@gmail.com>
2022-09-26 00:40:55 +0200

    spawn: more spam control over the spawning process

--- ballsdex/packages/countryballs/spawn.py
+++ ballsdex/packages/countryballs/spawn.py
@@ -1,6 +1,7 @@
 import discord
 import random
 import logging
+import asyncio
 
 from typing import cast
 from datetime import datetime
@@ -15,10 +16,18 @@ SPAWN_CHANCE_RANGE = (40, 140)
 
 @dataclass
 class SpawnCooldown:
-    author: int
     time: datetime
     amount: int = field(default=1)
     chance: int = field(default_factory=lambda: random.randint(*SPAWN_CHANCE_RANGE))
+    lock: asyncio.Lock = field(default_factory=asyncio.Lock, init=False)
+
+    async def increase(self) -> bool:
+        if self.lock.locked():
+            return False
+        async with self.lock:
+            self.chance += 1
+            await asyncio.sleep(4)
+        return True
 
 
 @dataclass
@@ -30,21 +39,32 @@ class SpawnManager:
         guild = message.guild
         if not guild:
             return
+
         cooldown = self.cooldowns.get(guild.id, None)
         if not cooldown:
-            cooldown = SpawnCooldown(message.author.id, message.created_at)
+            cooldown = SpawnCooldown(message.created_at)
             self.cooldowns[guild.id] = cooldown
             log.debug(f"Created cooldown manager for guild {guild.id}")
+
         delta = (message.created_at - cooldown.time).total_seconds()
-        if cooldown.author == message.author.id:
-            if delta < 10:
-                log.debug(f"Handled message {message.id}, cooldown ignore")
-                return
-        chance = cooldown.chance - (delta // 30)
-        cooldown.amount += 1
+        chance = cooldown.chance - (delta // 60)
+
+        # manager cannot be increased more than once per 5 seconds
+        if not await cooldown.increase():
+            log.debug(f"Handled message {message.id}, skipping due to spam control")
+            return
+
+        # normal increase, need to reach goal
         if cooldown.amount <= chance:
             log.debug(f"Handled message {message.id}, count: {cooldown.amount}/{chance}")
             return
+
+        # at this point, the goal is reached
+        if delta < 120:
+            # wait for at least 2 minutes before spawning
+            log.debug(f"Handled message {message.id}, waiting for manager to be 2 mins old")
+            return
+
         # spawn countryball
         del self.cooldowns[guild.id]
         log.debug(f"Handled message {message.id}, spawning ball")
