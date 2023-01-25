90b84ef749feac7e0c86aaaaa11a6ba195b0b877
retke <laggron42@gmail.com>
2023-01-25 22:46:12 +0100

    transformers: add BallTransformer and SpecialTransformer

--- ballsdex/core/utils/transformers.py
+++ ballsdex/core/utils/transformers.py
@@ -7,24 +7,25 @@ from discord.ext import tasks
 from datetime import datetime
 from dataclasses import dataclass
 from tortoise.exceptions import DoesNotExist
-from typing import AsyncIterator
+from typing import TypeVar, Generic, AsyncIterator
 
-from ballsdex.core.models import BallInstance, Player
+from ballsdex.core.models import Ball, BallInstance, Player, Special
 
 log = logging.getLogger("ballsdex.core.utils.transformers")
 
 CACHE_TIME = 30
+T = TypeVar("T")
 
 
 @dataclass
-class UserCountryballsCache:
+class ListCache(Generic[T]):
     time: float
-    balls: list[BallInstance]
+    balls: list[T]
 
 
-class CountryballCache:
+class BallInstanceCache:
     def __init__(self):
-        self.cache: dict[int, UserCountryballsCache] = {}
+        self.cache: dict[int, ListCache[BallInstance]] = {}
         self.clear_cache.start()
 
     async def get(self, user: discord.abc.User, value: str) -> AsyncIterator[BallInstance]:
@@ -40,7 +41,7 @@ class CountryballCache:
                 balls = []
             else:
                 balls = await BallInstance.filter(player=player).select_related("ball").all()
-            cache = UserCountryballsCache(time, balls)
+            cache = ListCache(time, balls)
             self.cache[user.id] = cache
 
         total = 0
@@ -64,7 +65,7 @@ class CountryballCache:
 
 class BallInstanceTransformer(app_commands.Transformer):
     def __init__(self):
-        self.cache = CountryballCache()
+        self.cache = BallInstanceCache()
 
     async def autocomplete(
         self, interaction: discord.Interaction, value: str
@@ -107,3 +108,83 @@ class BallInstanceTransformer(app_commands.Transformer):
 
 
 BallInstanceTransform = app_commands.Transform[BallInstance, BallInstanceTransformer]
+
+
+class BallTransformer(app_commands.Transformer):
+    def __init__(self):
+        self.cache: ListCache[Ball] | None = None
+
+    async def load_cache(self):
+        balls = await Ball.all()
+        self.cache = ListCache(time.time(), balls)
+
+    async def autocomplete(
+        self, interaction: discord.Interaction, value: str
+    ) -> list[app_commands.Choice[int | float | str]]:
+        if self.cache is None or time.time() - self.cache.time > 300:
+            await self.load_cache()
+        choices: list[app_commands.Choice] = []
+        for ball in self.cache.balls:
+            if value.lower() in ball.country.lower():
+                choices.append(app_commands.Choice(name=ball.country, value=str(ball.pk)))
+                if len(choices) == 25:
+                    break
+        return choices
+
+    async def transform(self, interaction: discord.Interaction, value: str) -> Ball | None:
+        if not value:
+            await interaction.response.send_message(
+                "You need to use the autocomplete function for the ball selection."
+            )
+            return None
+        try:
+            return await Ball.get(pk=int(value))
+        except (ValueError, DoesNotExist):
+            await interaction.response.send_message(
+                "The ball could not be found. Make sure to use the autocomplete "
+                "function on this command."
+            )
+            return None
+
+
+BallTransform = app_commands.Transform[Ball, BallTransformer]
+
+
+class SpecialTransformer(app_commands.Transformer):
+    def __init__(self):
+        self.cache: ListCache[Special] | None = None
+
+    async def load_cache(self):
+        events = await Special.all()
+        self.cache = ListCache(time.time(), events)
+
+    async def autocomplete(
+        self, interaction: discord.Interaction, value: str
+    ) -> list[app_commands.Choice[int | float | str]]:
+        if self.cache is None or time.time() - self.cache.time > 300:
+            await self.load_cache()
+        choices: list[app_commands.Choice] = []
+        for event in self.cache.balls:
+            if value.lower() in event.name.lower():
+                choices.append(app_commands.Choice(name=event.name, value=str(event.pk)))
+                if len(choices) == 25:
+                    break
+        return choices
+
+    async def transform(self, interaction: discord.Interaction, value: str) -> Ball | None:
+        if not value:
+            await interaction.response.send_message(
+                "You need to use the autocomplete function for the special background selection."
+            )
+            return None
+        try:
+            return await Ball.get(pk=int(value))
+        except (ValueError, DoesNotExist):
+            await interaction.response.send_message(
+                "The special event could not be found. Make sure to use the autocomplete "
+                "function on this command."
+            )
+            return None
+
+
+BallTransform = app_commands.Transform[Ball, BallTransformer]
