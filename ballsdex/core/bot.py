506bf46aeb7b38ae0e3aa4a04d1bbcdc16d5f954
retke <laggron42@gmail.com>
2023-02-24 11:35:17 +0100

    bot: fix bug in interaction check
    Would not block autocomplete calls

--- ballsdex/core/bot.py
+++ ballsdex/core/bot.py
@@ -33,7 +33,7 @@ class CommandTree(app_commands.CommandTree):
                     f"({round((len(bot.shards)/bot.shard_count)*100)}%)",
                     ephemeral=True,
                 )
-                return False  # wait for all shards to be connected
+            return False  # wait for all shards to be connected
         return await bot.blacklist_check(interaction)
 
 
