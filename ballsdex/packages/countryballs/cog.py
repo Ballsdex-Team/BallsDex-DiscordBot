fecfc6f78fde8c7fde6c9c4d76f5f8feb4ff05fc
laggron42 <laggron42@gmail.com>
2023-03-15 11:40:09 +0900

    countryballs: remove outdated text commands

--- ballsdex/packages/countryballs/cog.py
+++ ballsdex/packages/countryballs/cog.py
@@ -4,12 +4,9 @@ import logging
 from typing import Optional
 from tortoise.exceptions import DoesNotExist
 from discord.ext import commands
-from discord.utils import format_dt
 
-from ballsdex.core.models import GuildConfig, Ball
+from ballsdex.core.models import GuildConfig
 from ballsdex.packages.countryballs.spawn import SpawnManager
-from ballsdex.packages.countryballs.countryball import CountryBall
-from ballsdex.packages.countryballs.components import CountryballNamePrompt
 
 log = logging.getLogger("ballsdex.packages.countryballs")
 
@@ -64,75 +61,3 @@ class CountryBallsSpawner(commands.Cog):
                 del self.spawn_manager.cache[guild.id]
             elif channel:
                 self.spawn_manager.cache[guild.id] = channel.id
-
-    @commands.command()
-    @commands.is_owner()
-    async def spawnball(
-        self,
-        ctx: commands.Context,
-        channel: discord.TextChannel | None = None,
-        *,
-        ball: str | None = None,
-    ):
-        """
-        Force spawn a countryball.
-        """
-        if not ball:
-            countryball = await CountryBall.get_random()
-        else:
-            try:
-                ball_model = await Ball.get(country__iexact=ball.lower())
-            except DoesNotExist:
-                await ctx.send("No such ball exists.")
-                return
-            countryball = CountryBall(ball_model)
-        await countryball.spawn(channel or ctx.channel)
-
-    @commands.command()
-    @commands.is_owner()
-    @commands.guild_only()
-    async def spawnstats(self, ctx: commands.Context):
-        """
-        Get details about countryball spawner.
-        """
-        assert ctx.guild
-        if ctx.guild.id not in self.spawn_manager.cache:
-            await ctx.send("That guild does not have a registered spawn channel.")
-            return
-        cooldown_manager = self.spawn_manager.cooldowns[ctx.guild.id]
-        delta = (ctx.message.created_at - cooldown_manager.time).total_seconds()
-        await ctx.send(
-            f"Initiated {format_dt(cooldown_manager.time, style='R')}\n"
-            f"Counter: {cooldown_manager.amount}/{cooldown_manager.chance - (delta // 60)}"
-        )
-
-    @commands.command()
-    @commands.is_owner()
-    async def giveball(self, ctx: commands.Context, ball: str, *users: discord.User):
-        """
-        Give a countryball to one or more users.
-
-        If the countryball name has spaces, put it between quotes.
-        Multiple users may be given afterwards (mention or ID)
-        """
-        if not users:
-            await ctx.send("No users were provided.")
-            return
-        try:
-            ball_model = await Ball.get(country__iexact=ball.lower())
-        except DoesNotExist:
-            await ctx.send("No such ball exists.")
-            return
-
-        # reusing the catch function from components.py
-        # this is ugly, this will change later I hope with the queries rewrite
-        fake_prompt = CountryballNamePrompt(CountryBall(ball_model), None)  # type: ignore
-        async with ctx.typing():
-            for user in users:
-                await fake_prompt.catch_ball(ctx.bot, user)
-        if len(users) > 1:
-            await ctx.send(
-                f'The "{ball_model.country}" countryball was given to {len(users)} users.'
-            )
-        else:
-            await ctx.send(f'The "{ball_model.country}" countryball was given to {users[0]}.')
