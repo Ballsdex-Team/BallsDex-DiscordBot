5a4b407d780e256385e7da2ae3f6b3bfc5707f0f
laggron42 <laggron42@gmail.com>
2023-03-24 16:26:39 +0900

    bot: log how many shards are needed

--- ballsdex/core/bot.py
+++ ballsdex/core/bot.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import discord
 import logging
+import yarl
 
 from rich import print
 from typing import cast
@@ -9,6 +10,7 @@ from datetime import datetime
 
 from discord import app_commands
 from discord.ext import commands
+from discord.gateway import DiscordWebSocket
 
 from ballsdex.core.dev import Dev
 from ballsdex.core.metrics import PrometheusServer
@@ -112,6 +114,30 @@ class BallsDexBot(commands.AutoShardedBot):
     async def load_special_cache(self):
         now = datetime.now()
         self.special_cache = await Special.filter(start_date__lte=now, end_date__gt=now)
+
+    async def launch_shards(self) -> None:
+        # override to add a log call on the number of shards that needs connecting
+        if self.is_closed():
+            return
+
+        if self.shard_count is None:
+            self.shard_count: int
+            self.shard_count, gateway_url = await self.http.get_bot_gateway()
+            log.info(
+                f"Logged in to Discord, initiating connection. {self.shard_count} shards needed"
+            )
+            gateway = yarl.URL(gateway_url)
+        else:
+            gateway = DiscordWebSocket.DEFAULT_GATEWAY
+
+        self._connection.shard_count = self.shard_count
+
+        shard_ids = self.shard_ids or range(self.shard_count)
+        self._connection.shard_ids = shard_ids
+
+        for shard_id in shard_ids:
+            initial = shard_id == shard_ids[0]
+            await self.launch_shard(gateway, shard_id, initial=initial)
 
     async def on_ready(self):
         assert self.user
