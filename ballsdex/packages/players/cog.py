0a4852294771979a660b4d0bb93cad5ecf4e4267
laggron42 <laggron42@gmail.com>
2023-03-26 13:52:13 +0900

    players: donation policy

--- ballsdex/packages/players/cog.py
+++ ballsdex/packages/players/cog.py
@@ -7,9 +7,10 @@ from collections import defaultdict
 from tortoise.exceptions import DoesNotExist
 
 from discord import app_commands
+from discord.ui import View, Button, button
 from discord.ext import commands
 
-from ballsdex.core.models import Ball, Player, BallInstance
+from ballsdex.core.models import Ball, Player, BallInstance, DonationPolicy
 from ballsdex.core.utils.transformers import BallInstanceTransform
 from ballsdex.packages.players.countryballs_paginator import CountryballsViewer
 
@@ -17,6 +18,58 @@ if TYPE_CHECKING:
     from ballsdex.core.bot import BallsDexBot
 
 log = logging.getLogger("ballsdex.packages.countryballs")
+
+
+class DonationRequest(View):
+    def __init__(
+        self, interaction: discord.Interaction, countryball: BallInstance, new_player: Player
+    ):
+        super().__init__(timeout=120)
+        self.original_interaction = interaction
+        self.countryball = countryball
+        self.new_player = new_player
+
+    async def interaction_check(self, interaction: discord.Interaction, /) -> bool:
+        if interaction.user.id != self.new_player.discord_id:
+            await interaction.response.send_message(
+                "You are not allowed to interact with this menu.", ephemeral=True
+            )
+            return False
+        return True
+
+    async def on_timeout(self):
+        for item in self.children:
+            item.disabled = True
+        await self.original_interaction.followup.edit_message("@original", view=self)
+
+    @button(
+        style=discord.ButtonStyle.success, emoji="\N{HEAVY CHECK MARK}\N{VARIATION SELECTOR-16}"
+    )
+    async def accept(self, interaction: discord.Interaction, button: Button):
+        self.stop()
+        for item in self.children:
+            item.disabled = True
+        self.countryball.trade_player = self.countryball.player
+        self.countryball.player = self.new_player
+        await self.countryball.save()
+        await interaction.response.edit_message(
+            content=interaction.message.content
+            + "\n\N{WHITE HEAVY CHECK MARK} The donation was accepted!",
+            view=self,
+        )
+
+    @button(
+        style=discord.ButtonStyle.danger,
+        emoji="\N{HEAVY MULTIPLICATION X}\N{VARIATION SELECTOR-16}",
+    )
+    async def deny(self, interaction: discord.Interaction, button: Button):
+        self.stop()
+        for item in self.children:
+            item.disabled = True
+        await interaction.response.edit_message(
+            content=interaction.message.content + "\n\N{CROSS MARK} The donation was denied.",
+            view=self,
+        )
 
 
 class SortingChoices(enum.Enum):
@@ -245,6 +298,45 @@ class Players(commands.GroupCog, group_name="balls"):
             )
 
     @app_commands.command()
+    @app_commands.choices(
+        policy=[
+            app_commands.Choice(name="Accept all donations", value=DonationPolicy.ALWAYS_ACCEPT),
+            app_commands.Choice(
+                name="Request your approval first", value=DonationPolicy.REQUEST_APPROVAL
+            ),
+            app_commands.Choice(name="Deny all donations", value=DonationPolicy.ALWAYS_DENY),
+        ]
+    )
+    async def donation_policy(
+        self, interaction: discord.Interaction, policy: app_commands.Choice[int]
+    ):
+        """
+        Chance how you want to receive donations from /balls give
+
+        policy: DonationPolicy
+            The new policy for accepting donations
+        """
+        player, _ = await Player.get_or_create(discord_id=interaction.user.id)
+        player.donation_policy = policy.value
+        if policy.value == DonationPolicy.ALWAYS_ACCEPT:
+            await interaction.response.send_message(
+                "Setting updated, you will now receive all donated countryballs immediately."
+            )
+        elif policy.value == DonationPolicy.REQUEST_APPROVAL:
+            await interaction.response.send_message(
+                "Setting updated, you will now have to approve donation requests manually."
+            )
+        elif policy.value == DonationPolicy.ALWAYS_DENY:
+            await interaction.response.send_message(
+                f"Setting updated, it is now impossible to use {self.give.extras['mention']} with "
+                "you. It is still possible to perform donations using the trade system."
+            )
+        else:
+            await interaction.response.send_message("Invalid input!")
+            return
+        await player.save()  # do not save if the input is invalid
+
+    @app_commands.command()
     async def give(
         self,
         interaction: discord.Interaction,
@@ -266,8 +358,6 @@ class Players(commands.GroupCog, group_name="balls"):
         if user.bot:
             await interaction.response.send_message("You cannot donate to bots.")
             return
-        await interaction.response.send_message("This command is temporarily disabled.")
-        return
 
         new_player, _ = await Player.get_or_create(discord_id=user.id)
         old_player = countryball.player
@@ -275,6 +365,18 @@ class Players(commands.GroupCog, group_name="balls"):
         if new_player == old_player:
             await interaction.response.send_message("You cannot give a ball to yourself.")
             return
+        if new_player.donation_policy == DonationPolicy.ALWAYS_DENY:
+            await interaction.response.send_message(
+                "This player does not accept donations. You can use trades instead."
+            )
+            return
+        elif new_player.donation_policy == DonationPolicy.REQUEST_APPROVAL:
+            await interaction.response.send_message(
+                f"Hey {user.mention}, {interaction.user.name} wants to give you "
+                f"{countryball.description(include_emoji=True, bot=interaction.client)}!\n"
+                "Do you accept this donation?",
+                view=DonationRequest(interaction, countryball, new_player),
+            )
 
         countryball.player = new_player
         countryball.trade_player = old_player
