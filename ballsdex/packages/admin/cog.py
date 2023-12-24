943a3a37498a98d9f56e83c96a75913e116fff66
LeMx <153226500+MaxxMXSZ@users.noreply.github.com>
2023-12-24 21:33:26 +0100

    optional*

--- ballsdex/packages/admin/cog.py
+++ ballsdex/packages/admin/cog.py
@@ -3,7 +3,7 @@ import random
 import re
 from collections import defaultdict
 from pathlib import Path
-from typing import TYPE_CHECKING, cast, Option
+from typing import TYPE_CHECKING, cast, Optional
 
 import discord
 from discord import app_commands
