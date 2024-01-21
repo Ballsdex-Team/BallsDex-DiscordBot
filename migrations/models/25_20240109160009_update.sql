e146616ff9e3c441ac589d8592b073a393f0a152
Auguste "laggron42" Charpentier <laggron42@gmail.com>
2024-01-21 03:28:38 +0100

    migrations: better query, thanks preda

--- migrations/models/25_20240109160009_update.sql
+++ migrations/models/25_20240109160009_update.sql
@@ -1,12 +1,12 @@
 -- upgrade --
 ALTER TABLE "ball" ADD "created_at" TIMESTAMPTZ   DEFAULT CURRENT_TIMESTAMP;
 UPDATE ball b
-SET created_at = (
-  SELECT inst.catch_date
-  FROM ballinstance inst
-  WHERE inst.ball_id = b.id
-  ORDER BY inst.catch_date ASC
-  LIMIT 1
-);
+SET created_at = bi.catch_date
+FROM (
+  SELECT ball_id, MIN(catch_date) AS catch_date
+  FROM ballinstance
+  GROUP BY ball_id
+) AS bi
+WHERE b.id = bi.ball_id;
 -- downgrade --
 ALTER TABLE "ball" DROP COLUMN "created_at";
