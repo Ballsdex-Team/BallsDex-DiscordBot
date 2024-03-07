43917c3db00d09215de4c4d0f852d24e7e54415b
Jamie <31554168+flaree@users.noreply.github.com>
2024-03-07 15:06:50 +0100

    trade: optional user arg to history (#211)
    * Add optional user arg to trade history
    
    * Avoid repetitions
    
    ---------
    
    Co-authored-by: Auguste "laggron42" Charpentier <laggron42@gmail.com>

--- ballsdex/packages/trade/cog.py
+++ ballsdex/packages/trade/cog.py
@@ -258,18 +258,31 @@ class Trade(commands.GroupCog):
         self,
         interaction: discord.Interaction["BallsDexBot"],
         sorting: app_commands.Choice[str],
+        trade_user: discord.User | None = None,
     ):
         """
         Show the history of your trades.
+
+        Parameters
+        ----------
+        sorting: str
+            The sorting order of the trades
+        trade_user: discord.User | None
+            The user you want to see your trade history with
         """
         await interaction.response.defer(ephemeral=True, thinking=True)
         user = interaction.user
-        history = (
-            await TradeModel.filter(
+        if trade_user:
+            history_queryset = TradeModel.filter(
+                Q(player1__discord_id=user.id, player2__discord_id=trade_user.id)
+                | Q(player1__discord_id=trade_user.id, player2__discord_id=user.id)
+            )
+        else:
+            history_queryset = TradeModel.filter(
                 Q(player1__discord_id=user.id) | Q(player2__discord_id=user.id)
             )
-            .order_by(sorting.value)
-            .prefetch_related("player1", "player2")
+        history = await history_queryset.order_by(sorting.value).prefetch_related(
+            "player1", "player2"
         )
         if not history:
             await interaction.followup.send("No history found.", ephemeral=True)
