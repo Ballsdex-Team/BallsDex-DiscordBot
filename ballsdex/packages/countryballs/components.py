a15e8a0e1f71faef68ed8eeddb5b024e08568929
LeMx <153226500+MaxxMXSZ@users.noreply.github.com>
2024-03-23 21:09:38 +0100

    Nicer
    laggron requested

--- ballsdex/packages/countryballs/components.py
+++ ballsdex/packages/countryballs/components.py
@@ -77,8 +77,8 @@ class CountryballNamePrompt(Modal, title=f"Catch this {settings.collectible_name
                 )
 
             f"{interaction.user.mention} You caught **{self.ball.name}!** "
-            f"`({ball.attack_bonus:+}%/{ball.health_bonus:+}%)` "
-            f"(`#{ball.pk:0X}`)\n\n{special}"
+            f"`(#{ball.pk:0X}, {ball.attack_bonus:+}%/{ball.health_bonus:+}%)`\n\n"
+            f"{special}"
             )
             self.button.disabled = True
             await interaction.followup.edit_message(self.ball.message.id, view=self.button.view)
