2d47536b395df871621ba0ff2a9392a0980e1674
Jamie <31554168+flaree@users.noreply.github.com>
2024-04-19 14:28:26 +0100

    [players] add delete and export command (#236)
    * [players] add delete and export command
    
    * remove excess message
    
    * check if player is None
    
    * check zip size instead
    
    * defer and ignore
    
    * Update cog.py
    
    Co-authored-by: MAX <63972751+ltzmax@users.noreply.github.com>
    
    * Already been responded from before
    
    * Update to send the file to dm.
    
    * Style
    
    * Update to what flare said
    
    * Style
    
    * Update cog
    
    ---------
    
    Co-authored-by: MAX <63972751+ltzmax@users.noreply.github.com>

--- ballsdex/packages/players/cog.py
+++ ballsdex/packages/players/cog.py
@@ -1,12 +1,16 @@
+import zipfile
+from io import BytesIO
 from typing import TYPE_CHECKING
 
 import discord
 from discord import app_commands
 from discord.ext import commands
+from tortoise.expressions import Q
 
-from ballsdex.core.models import DonationPolicy
+from ballsdex.core.models import BallInstance, DonationPolicy
 from ballsdex.core.models import Player as PlayerModel
-from ballsdex.core.models import PrivacyPolicy
+from ballsdex.core.models import PrivacyPolicy, Trade, TradeObject
+from ballsdex.core.utils.buttons import ConfirmChoiceView
 from ballsdex.settings import settings
 
 if TYPE_CHECKING:
@@ -91,3 +95,130 @@ class Player(commands.GroupCog):
             await interaction.response.send_message("Invalid input!")
             return
         await player.save()  # do not save if the input is invalid
+
+    @app_commands.command()
+    async def delete(self, interaction: discord.Interaction):
+        """
+        Delete your player data.
+        """
+        view = ConfirmChoiceView(interaction)
+        await interaction.response.send_message(
+            "Are you sure you want to delete your player data?", view=view, ephemeral=True
+        )
+        await view.wait()
+        if view.value is None or not view.value:
+            return
+        player, _ = await PlayerModel.get_or_create(discord_id=interaction.user.id)
+        await player.delete()
+
+    @app_commands.command()
+    @app_commands.choices(
+        type=[
+            app_commands.Choice(name=settings.collectible_name.title(), value="balls"),
+            app_commands.Choice(name="Trades", value="trades"),
+            app_commands.Choice(name="All", value="all"),
+        ]
+    )
+    async def export(self, interaction: discord.Interaction, type: str):
+        """
+        Export your player data.
+        """
+        player = await PlayerModel.get_or_none(discord_id=interaction.user.id)
+        if player is None:
+            await interaction.response.send_message(
+                "You don't have any player data to export.", ephemeral=True
+            )
+            return
+        await interaction.response.defer()
+        files = []
+        if type == "balls":
+            data = await get_items_csv(player)
+            filename = f"{interaction.user.id}_{settings.collectible_name}.csv"
+            data.filename = filename  # type: ignore
+            files.append(data)
+        elif type == "trades":
+            data = await get_trades_csv(player)
+            filename = f"{interaction.user.id}_trades.csv"
+            data.filename = filename  # type: ignore
+            files.append(data)
+        elif type == "all":
+            balls = await get_items_csv(player)
+            trades = await get_trades_csv(player)
+            balls_filename = f"{interaction.user.id}_{settings.collectible_name}.csv"
+            trades_filename = f"{interaction.user.id}_trades.csv"
+            balls.filename = balls_filename  # type: ignore
+            trades.filename = trades_filename  # type: ignore
+            files.append(balls)
+            files.append(trades)
+        else:
+            await interaction.followup.send("Invalid input!", ephemeral=True)
+            return
+        zip_file = BytesIO()
+        with zipfile.ZipFile(zip_file, "w") as z:
+            for file in files:
+                z.writestr(file.filename, file.getvalue())
+        zip_file.seek(0)
+        if zip_file.tell() > 25_000_000:
+            await interaction.followup.send(
+                "Your data is too large to export."
+                "Please contact the bot support for more information.",
+                ephemeral=True,
+            )
+            return
+        files = [discord.File(zip_file, "player_data.zip")]
+        try:
+            await interaction.user.send("Here is your player data:", files=files)
+            await interaction.followup.send(
+                "Your player data has been sent via DMs.", ephemeral=True
+            )
+        except discord.Forbidden:
+            await interaction.followup.send(
+                "I couldn't send the player data to you in DM. "
+                "Either you blocked me or you disabled DMs in this server.",
+                ephemeral=True,
+            )
+
+
+async def get_items_csv(player: PlayerModel) -> BytesIO:
+    """
+    Get a CSV file with all items of the player.
+    """
+    balls = await BallInstance.filter(player=player).prefetch_related(
+        "ball", "trade_player", "special"
+    )
+    txt = (
+        f"id,hex id,{settings.collectible_name},catch date,trade_player"
+        ",special,shiny,attack,attack bonus,hp,hp_bonus\n"
+    )
+    for ball in balls:
+        txt += (
+            f"{ball.id},{ball.id:0X},{ball.ball.country},{ball.catch_date},"  # type: ignore
+            f"{ball.trade_player.discord_id if ball.trade_player else 'None'},{ball.special},"
+            f"{ball.shiny},{ball.attack},{ball.attack_bonus},{ball.health},{ball.health_bonus}\n"
+        )
+    return BytesIO(txt.encode("utf-8"))
+
+
+async def get_trades_csv(player: PlayerModel) -> BytesIO:
+    """
+    Get a CSV file with all trades of the player.
+    """
+    trade_history = (
+        await Trade.filter(Q(player1=player) | Q(player2=player))
+        .order_by("date")
+        .prefetch_related("player1", "player2")
+    )
+    txt = "id,date,player1,player2,player1 received,player2 received\n"
+    for trade in trade_history:
+        player1_items = await TradeObject.filter(
+            trade=trade, player=trade.player1
+        ).prefetch_related("ballinstance")
+        player2_items = await TradeObject.filter(
+            trade=trade, player=trade.player2
+        ).prefetch_related("ballinstance")
+        txt += (
+            f"{trade.id},{trade.date},{trade.player1.discord_id},{trade.player2.discord_id},"
+            f"{','.join([i.ballinstance.to_string() for i in player2_items])},"  # type: ignore
+            f"{','.join([i.ballinstance.to_string() for i in player1_items])}\n"  # type: ignore
+        )
+    return BytesIO(txt.encode("utf-8"))
