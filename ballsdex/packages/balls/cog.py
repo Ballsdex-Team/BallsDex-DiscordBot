66ce1cc2e03b2c3930ecb2543accb199664d1dfc
imtherealF1 <168587794+imtherealF1@users.noreply.github.com>
2024-06-07 13:42:45 +0300

    small space fix
    it pretty much made the special description all go to the countryball description, sry about that

--- ballsdex/packages/balls/cog.py
+++ ballsdex/packages/balls/cog.py
@@ -475,7 +475,7 @@ class Balls(commands.GroupCog, group_name=settings.players_group_cog_name):
             The user you want to give a countryball to
         countryball: BallInstance
             The countryball you're giving away
-         special: Special
+        special: Special
             Filter the results of autocompletion to a special event. Ignored afterwards.
         shiny: bool
             Filter the results of autocompletion to shinies. Ignored afterwards.
