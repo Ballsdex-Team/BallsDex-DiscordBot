f6e14bb2bc1b5723a4e89657b0480c00c182ece0
Jamie <31554168+flaree@users.noreply.github.com>
2024-08-18 19:45:26 +0100

    [balls] add stats to balls give (#326)
    * fix description logic in ball description
    
    * Add sats to balls give
    
    * revert model change
    
    * final revert

--- ballsdex/packages/balls/cog.py
+++ ballsdex/packages/balls/cog.py
@@ -552,7 +552,7 @@ class Balls(commands.GroupCog, group_name=settings.players_group_cog_name):
         elif new_player.donation_policy == DonationPolicy.REQUEST_APPROVAL:
             await interaction.followup.send(
                 f"Hey {user.mention}, {interaction.user.name} wants to give you "
-                f"{countryball.description(include_emoji=True, bot=self.bot, is_trade=True)}!\n"
+                f"{countryball.description(include_emoji=True, bot=self.bot, is_trade=True)} (`{countryball.attack_bonus:+}%/{countryball.health_bonus:+}%`)!\n"
                 "Do you accept this donation?",
                 view=DonationRequest(self.bot, interaction, countryball, new_player),
             )
