4319405b234f9ac4803455f30141e08edb1c7878
imtherealF1 <168587794+imtherealF1@users.noreply.github.com>
2024-08-22 13:53:17 +0300

    adding interaction check to config embed and small logic changes

--- ballsdex/packages/config/components.py
+++ ballsdex/packages/config/components.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ui import Button, View, button
 
-from ballsdex.core.models import GuildConfig
+from ballsdex.core.models import GuildConfig, Player
 from ballsdex.settings import settings
 
 
@@ -10,10 +10,18 @@ class AcceptTOSView(View):
     Button prompting the admin setting up the bot to accept the terms of service.
     """
 
-    def __init__(self, interaction: discord.Interaction, channel: discord.TextChannel):
+    def __init__(
+        self,
+        interaction: discord.Interaction,
+        channel: discord.TextChannel,
+        new_player: Player,
+    ):
         super().__init__()
         self.original_interaction = interaction
         self.channel = channel
+        self.new_player = new_player
+        self.message = None
+
         self.add_item(
             Button(
                 style=discord.ButtonStyle.link,
@@ -29,6 +37,14 @@ class AcceptTOSView(View):
             )
         )
 
+    async def interaction_check(self, interaction: discord.Interaction) -> bool:
+        if interaction.user.id != self.new_player.discord_id:
+            await interaction.response.send_message(
+                "You are not allowed to interact with this menu.", ephemeral=True
+            )
+            return False
+        return True
+
     @button(
         label="Accept",
         style=discord.ButtonStyle.success,
@@ -42,27 +58,29 @@ class AcceptTOSView(View):
             "ballsdex_settings_change", interaction.guild, channel=self.channel
         )
         self.stop()
+        if self.message:
+            item.disabled = True
+            try:
+                await self.message.edit(view=self)  # Update the original message
+            except discord.HTTPException:
+                pass
         await interaction.response.send_message(
             f"The new spawn channel was successfully set to {self.channel.mention}.\n"
             f"{settings.collectible_name.title()}s will start spawning as"
             " users talk unless the bot is disabled."
         )
 
-        self.accept_button.disabled = True
         try:
-            await self.original_interaction.followup.edit_message(
-                "@original", view=self  # type: ignore
-            )
+            await self.message.edit(view=self)
         except discord.HTTPException:
             pass
 
     async def on_timeout(self) -> None:
         self.stop()
-        for item in self.children:
-            item.disabled = True  # type: ignore
-        try:
-            await self.original_interaction.followup.edit_message(
-                "@original", view=self  # type: ignore
-            )
-        except discord.HTTPException:
-            pass
+        if self.message:
+            for item in self.children:
+                item.disabled = True
+            try:
+                await self.message.edit(view=self)  # Update the original message
+            except discord.HTTPException:
+                pass
