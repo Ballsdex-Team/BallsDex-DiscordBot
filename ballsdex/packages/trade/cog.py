ed2fb214ee4f0783f836ea8a1946f3e803bb80da
imtherealF1 <168587794+imtherealF1@users.noreply.github.com>
2024-08-30 13:41:53 +0300

    [trade] fix countryball history arguement showing all history (#354)
    * fixing trade history sort countryball
    
    * changing the code for checks to pass

--- ballsdex/packages/trade/cog.py
+++ ballsdex/packages/trade/cog.py
@@ -399,16 +399,16 @@ class Trade(commands.GroupCog):
             queryset = queryset.filter(date__range=(start_date, end_date))
 
         if countryball:
-            queryset = queryset.filter(
-                Q(player1__tradeobjects__ballinstance__ball=countryball)
-                | Q(player2__tradeobjects__ballinstance__ball=countryball)
-            ).distinct()  # for some reason, this query creates a lot of duplicate rows?
+            queryset = queryset.filter(Q(tradeobjects__ballinstance__ball=countryball)).distinct()
 
-        history = await queryset.order_by(sorting.value).prefetch_related("player1", "player2")
+        history = await queryset.order_by(sorting.value).prefetch_related(
+            "player1", "player2", "tradeobjects__ballinstance__ball"
+        )
 
         if not history:
             await interaction.followup.send("No history found.", ephemeral=True)
             return
+
         source = TradeViewFormat(history, interaction.user.name, self.bot)
         pages = Pages(source=source, interaction=interaction)
         await pages.start()
