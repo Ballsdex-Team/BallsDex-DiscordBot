d3db1b6a630152fd32ab55f1c2cb8fe5ef8200ef
imtherealF1 <168587794+imtherealF1@users.noreply.github.com>
2024-08-31 19:09:26 +0300

    reverting unnecessary changes

--- ballsdex/packages/trade/menu.py
+++ ballsdex/packages/trade/menu.py
@@ -422,39 +422,34 @@ class CountryballsSelector(Pages):
         await interaction.response.defer(thinking=True, ephemeral=True)
         trade, trader = self.cog.get_trade(interaction)
         if trade is None or trader is None:
-            await interaction.followup.send(
+            return await interaction.followup.send(
                 "The trade has been cancelled or the user is not part of the trade.",
                 ephemeral=True,
             )
-            return
         if trader.locked:
-            await interaction.followup.send(
+            return await interaction.followup.send(
                 "You have locked your proposal, it cannot be edited! "
                 "You can click the cancel button to stop the trade instead.",
                 ephemeral=True,
             )
-            return
         if any(ball in trader.proposal for ball in self.balls_selected):
-            await interaction.followup.send(
+            return await interaction.followup.send(
                 f"You have already added some of the {settings.collectible_name}s you selected.",
                 ephemeral=True,
             )
-            return
 
         if len(self.balls_selected) == 0:
-            await interaction.followup.send(
+            return await interaction.followup.send(
                 f"You have not selected any {settings.collectible_name}s "
                 "to add to your proposal.",
                 ephemeral=True,
             )
-            return
         for ball in self.balls_selected:
             if ball.is_tradeable is False:
-                await interaction.followup.send(
+                return await interaction.followup.send(
                     f"{settings.collectible_name.title()} #{ball.pk:0X} is not tradeable.",
                     ephemeral=True,
                 )
-                return
             trader.proposal.append(ball)
             await ball.lock_for_trade()
         grammar = "" if len(self.balls_selected) == 1 else "s"
@@ -480,8 +475,7 @@ class CountryballsSelector(Pages):
 
 class BulkAddView(CountryballsSelector):
     async def on_timeout(self) -> None:
-        await super().on_timeout()
-        return
+        return await super().on_timeout()
 
 
 class TradeViewSource(menus.ListPageSource):
@@ -529,21 +523,19 @@ class TradeViewMenu(Pages):
         player = await Player.get(discord_id=int(item.values[0]))
         trade, trader = self.cog.get_trade(interaction)
         if trade is None or trader is None:
-            await interaction.followup.send(
+            return await interaction.followup.send(
                 "The trade has been cancelled or the user is not part of the trade.",
                 ephemeral=True,
             )
-            return
         trade_player = (
             trade.trader1 if trade.trader1.user.id == player.discord_id else trade.trader2
         )
         ball_instances = trade_player.proposal
         if len(ball_instances) == 0:
-            await interaction.followup.send(
+            return await interaction.followup.send(
                 f"{trade_player.user} has not added any {settings.collectible_name}s.",
                 ephemeral=True,
             )
-            return
 
         await interaction.response.defer(thinking=True)
         paginator = CountryballsViewer(interaction, ball_instances)
