2e74ceca4ed1ce9e814f5cf9ad6492c69bea7850
imtherealF1 <168587794+imtherealF1@users.noreply.github.com>
2024-09-07 01:09:26 +0300

    removing unnecessary import

--- ballsdex/packages/players/cog.py
+++ ballsdex/packages/players/cog.py
@@ -5,7 +5,6 @@ from typing import TYPE_CHECKING
 import discord
 from discord import app_commands
 from discord.ext import commands
-from tortoise.exceptions import DoesNotExist
 from tortoise.expressions import Q
 
 from ballsdex.core.models import BallInstance, DonationPolicy
