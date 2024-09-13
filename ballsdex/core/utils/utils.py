02f6c591eda0e386d3c1175fb487739acf9595cc
flaree <flare2399@gmail.com>
2024-09-13 19:49:43 +0100

    precommit

--- ballsdex/core/utils/utils.py
+++ ballsdex/core/utils/utils.py
@@ -2,6 +2,7 @@ import discord
 
 from ballsdex.settings import settings
 
+
 def is_staff(interaction: discord.Interaction) -> bool:
     if interaction.guild and interaction.guild.id in settings.admin_guild_ids:
         roles = settings.admin_role_ids + settings.root_role_ids
