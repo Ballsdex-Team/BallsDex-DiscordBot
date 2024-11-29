1e30ddb475d188c1bcb825713433aaa3ed9d87c1
flaree <flare2399@gmail.com>
2024-11-29 12:25:22 +0000

    Move SortingChoice to seperate enum

--- ballsdex/core/utils/enums.py
+++ ballsdex/core/utils/enums.py
@@ -1,3 +1,5 @@
+import enum
+
 DONATION_POLICY_MAP = {
     1: "Accept all donations",
     2: "Approve donations",
@@ -10,3 +12,20 @@ PRIVATE_POLICY_MAP = {1: "Public", 2: "Private", 3: "Mutual Servers", 4: "Friend
 MENTION_POLICY_MAP = {1: "Allow all mentions", 2: "Deny all mentions"}
 
 FRIEND_POLICY_MAP = {1: "Allow all friend requests", 2: "Deny all friend requests"}
+
+
+class SortingChoices(enum.Enum):
+    alphabetic = "ball__country"
+    catch_date = "-catch_date"
+    rarity = "ball__rarity"
+    special = "special__id"
+    health = "health"
+    attack = "attack"
+    health_bonus = "-health_bonus"
+    attack_bonus = "-attack_bonus"
+    stats_bonus = "stats"
+    total_stats = "total_stats"
+
+    # manual sorts are not sorted by SQL queries but by our code
+    # this may be do-able with SQL still, but I don't have much experience ngl
+    duplicates = "manualsort-duplicates"
