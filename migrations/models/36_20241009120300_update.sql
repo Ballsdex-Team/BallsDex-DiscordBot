4927e0b781ab7a24db8c2965c9fdbad52adc6556
Auguste "laggron42" Charpentier <laggron42@gmail.com>
2024-12-02 21:19:39 +0100

    remove template stuff

--- migrations/models/36_20241009120300_update.sql
+++ migrations/models/36_20241009120300_update.sql
@@ -1,13 +1,11 @@
 -- upgrade --
-ALTER TABLE "regime" ADD "template" VARCHAR(64);
-ALTER TABLE "special" ADD "template" VARCHAR(64);
 ALTER TABLE "special" ALTER COLUMN "start_date" DROP NOT NULL;
 ALTER TABLE "special" ALTER COLUMN "end_date" DROP NOT NULL;
 
 WITH shiny_id AS (
-    INSERT INTO "special" (name, catch_phrase, rarity, emoji, background, template) VALUES
+    INSERT INTO "special" (name, catch_phrase, rarity, emoji, background) VALUES
     ('Shiny', '**It''s a shiny countryball!**', 0.00048828125,
-    '✨', '/ballsdex/core/image_generator/src/shiny.png', 'shiny')
+    '✨', '/ballsdex/core/image_generator/src/shiny.png')
     RETURNING id);
 UPDATE "ballinstance" SET "special_id" = shiny_id WHERE "shiny" = true;
 
@@ -22,7 +20,5 @@ AND s.name = 'Shiny' AND s.background = '/ballsdex/core/image_generator/src/shin
 DELETE FROM "special" WHERE
     name = 'Shiny' AND background = '/ballsdex/core/image_generator/src/shiny.png';
 
-ALTER TABLE "regime" DROP COLUMN "template";
-ALTER TABLE "special" DROP COLUMN "template";
 ALTER TABLE "special" ALTER COLUMN "start_date" SET NOT NULL;
 ALTER TABLE "special" ALTER COLUMN "end_date" SET NOT NULL;
