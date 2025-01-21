0c2673832926d89f9649cdcd24fd4c460cdac034
Auguste "laggron42" Charpentier <laggron42@gmail.com>
2025-01-21 14:11:40 +0100

    display special preview

--- admin_panel/preview/urls.py
+++ admin_panel/preview/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import render_image
+from .views import render_ballinstance, render_special
 
 urlpatterns = [
-    path("ball/generate/<int:ball_pk>", render_image),
+    path("ball/generate/<int:ball_pk>", render_ballinstance),
+    path("special/generate/<int:special_pk>", render_special),
 ]
