f4adffebfccdc1452209900048c66fbb11ee9c4d
Auguste "laggron42" Charpentier <laggron42@gmail.com>
2025-01-22 00:57:51 +0100

    admin_panel: redirect https for production social auth

--- admin_panel/admin_panel/settings/production.example.py
+++ admin_panel/admin_panel/settings/production.example.py
@@ -5,6 +5,9 @@ from .base import *
 
 DEBUG = False
 
+# Force python-social-auth (Discord OAuth2) to use https redirection
+SOCIAL_AUTH_REDIRECT_IS_HTTPS = True
+
 # Generate a long random string here for your secret key. It is important to keep it secret,
 # leaking it could allow attackers to do privilege escalation.
 # A good way to generate a long key is to run "pwgen 64 1" on Linux
