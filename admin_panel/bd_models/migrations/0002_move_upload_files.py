01da9daa9aa460f7918fc7a38f0a1ec79915e265
Auguste "laggron42" Charpentier <laggron42@gmail.com>
2025-02-12 15:36:32 +0100

    skip path migration when there is no table found

--- admin_panel/bd_models/migrations/0002_move_upload_files.py
+++ admin_panel/bd_models/migrations/0002_move_upload_files.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from typing import TYPE_CHECKING
 
 from bd_models.models import Ball, Economy, Regime, Special
-from django.db import migrations
+from django.db import connection, migrations
 from django.db.models import Case, ImageField, When
 from django.db.models.expressions import F, Value
 from django.db.models.functions import Concat, Replace
@@ -60,6 +60,10 @@ def _check_reserved_names():
 
 
 def move_forwards(apps: "Apps", schema_editor: "BaseDatabaseSchemaEditor"):
+    # only run this migration if there are existing tables (migrating from aerich)
+    if "ball" not in connection.introspection.table_names():
+        return
+
     # Run this first, as outdated objects are created by aerich's initial migrations
     Regime.objects.update(**_replace_text("background"))
     Special.objects.update(**_replace_text("background"))
@@ -80,6 +84,10 @@ def move_forwards(apps: "Apps", schema_editor: "BaseDatabaseSchemaEditor"):
 
 
 def move_backwards(apps: "Apps", schema_editor: "BaseDatabaseSchemaEditor"):
+    # only run this migration if the tables weren't deleted
+    if "ball" not in connection.introspection.table_names():
+        return
+
     Ball.objects.update(
         **_replace_text("wild_card", reverse=True),
         **_replace_text("collection_card", reverse=True),
