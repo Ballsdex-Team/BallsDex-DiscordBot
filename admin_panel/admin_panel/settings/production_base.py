67a76143db5379d3e9165ebc433fc2d8bd543723
Kowlin <10947836+Kowlin@users.noreply.github.com>
2025-02-12 14:51:18 +0100

    django: server sided security options (#529)
    * Add in more server sided security options
    
    * add typing and discord host
    
    * move middleware injection in production_base.py
    
    ---------
    
    Co-authored-by: Auguste "laggron42" Charpentier <laggron42@gmail.com>

--- admin_panel/admin_panel/settings/production_base.py
+++ admin_panel/admin_panel/settings/production_base.py
@@ -8,3 +8,6 @@ SOCIAL_AUTH_REDIRECT_IS_HTTPS = True
 # Correctly read the headers when using a proxy like nginx
 # Failing to configure this setting will result in CSRF errors in HTTPS
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
+# Adds security headers to our HTTP responses, see admin_panel/middleware.py for details
+MIDDLEWARE.append("admin_panel.middleware.SecurityHeadersMiddleware")
