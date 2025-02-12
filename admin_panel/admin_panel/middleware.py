1e0bce84c04473dcfc31eb17959f6f8a5bb506f4
Auguste "laggron42" Charpentier <laggron42@gmail.com>
2025-02-12 14:48:50 +0100

    add typing and discord host

--- admin_panel/admin_panel/middleware.py
+++ admin_panel/admin_panel/middleware.py
@@ -1,10 +1,18 @@
+from typing import TYPE_CHECKING, Callable
+
+if TYPE_CHECKING:
+    from django.http import HttpRequest, HttpResponse
+
+
 class SecurityHeadersMiddleware:
-    def __init__(self, get_response):
+    def __init__(self, get_response: Callable[["HttpRequest"], "HttpResponse"]):
         self.get_response = get_response
 
-    def __call__(self, request):
+    def __call__(self, request: HttpRequest) -> HttpResponse:
         response = self.get_response(request)
-        response["Content-Security-Policy"] = "default-src 'self'; frame-ancestors 'self'"
+        response["Content-Security-Policy"] = (
+            "default-src 'self' http://*.discordapp.com http://discord.com; frame-ancestors 'self'"
+        )
         response["X-XSS-Protection"] = "1; mode=block"
         response["X-Robots-Tag"] = "none"
         response["Referrer-Policy"] = "same-origin"
