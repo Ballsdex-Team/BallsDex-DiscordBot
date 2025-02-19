83f34b95a7404d28061129ed2a6a10eafdc2bf00
flaree <flare2399@gmail.com>
2025-02-19 14:33:13 +0000

    Add trade cooldown bypass policy

--- ballsdex/core/models.py
+++ ballsdex/core/models.py
@@ -417,6 +417,11 @@ class FriendPolicy(IntEnum):
     DENY = 2
 
 
+class TradeCooldownPolicy(IntEnum):
+    COOLDOWN = 1
+    BYPASS = 2
+
+
 class Player(models.Model):
     discord_id = fields.BigIntField(
         description="Discord user ID", unique=True, validators=[DiscordSnowflakeValidator()]
@@ -440,6 +445,11 @@ class Player(models.Model):
         FriendPolicy,
         description="How you want to handle friend requests",
         default=FriendPolicy.ALLOW,
+    )
+    trade_cooldown_policy = fields.IntEnumField(
+        TradeCooldownPolicy,
+        description="How you want to handle trade accept cooldown",
+        default=TradeCooldownPolicy.COOLDOWN,
     )
     balls: fields.BackwardFKRelation[BallInstance]
 
