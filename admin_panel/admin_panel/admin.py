c5d88532d08f39aab40957fffaf93d6afd057661
imtherealF1 <168587794+imtherealF1@users.noreply.github.com>
2025-04-23 17:46:27 +0300

    remove .title()
    Co-authored-by: Jamie <31554168+flaree@users.noreply.github.com>

--- admin_panel/admin_panel/admin.py
+++ admin_panel/admin_panel/admin.py
@@ -4,7 +4,7 @@ from ballsdex.settings import settings
 
 
 class BallsdexAdminSite(admin.AdminSite):
-    site_header = f"{settings.bot_name.title()} administration"
-    site_title = f"{settings.bot_name.title()} admin panel"
+    site_header = f"{settings.bot_name} administration"
+    site_title = f"{settings.bot_name} admin panel"
     site_url = None  # type: ignore
     final_catch_all_view = False
