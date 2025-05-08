6a9afdbc8ec86032e54c1d6ae99a5c745b80e498
Dormierian Citizen <dormieriancitizen@gmail.com>
2025-05-08 16:14:51 -0400

    newline support

--- ballsdex/core/image_generator/image_gen.py
+++ ballsdex/core/image_generator/image_gen.py
@@ -88,7 +88,15 @@ def draw_card(
             stroke_width=2,
             stroke_fill=(0, 0, 0, 255),
         )
-    for i, line in enumerate(textwrap.wrap(ball.capacity_description, width=32)):
+
+    capacity_description_lines = ball.capacity_description.split("\n")
+    capacity_description_lines = [
+        textwrap.wrap(line, width=32) for line in capacity_description_lines
+    ]
+    capacity_description_lines = [
+        line for sublist in capacity_description_lines for line in sublist
+    ]  # flatten list
+    for i, line in enumerate(capacity_description_lines):
         draw.text(
             (60, 1100 + 100 * len(cap_name) + 80 * i),
             line,
