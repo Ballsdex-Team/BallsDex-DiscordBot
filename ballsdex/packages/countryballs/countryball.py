f49150bb50f064b28a6c8fff8d6b9d6e49f0117c
dormieriancitizen <111535275+dormieriancitizen@users.noreply.github.com>
2025-05-12 05:33:43 -0400

    [countryballs] add optional wrong name variable (#611)
    * tell the user what the wrong name is
    
    * Make sure name isn't too long
    
    * formatting
    
    * Appease flake
    
    * Oops left comment in
    
    * Show name with ellipsis
    
    Co-authored-by: Jamie <31554168+flaree@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Jamie <31554168+flaree@users.noreply.github.com>

--- ballsdex/packages/countryballs/countryball.py
+++ ballsdex/packages/countryballs/countryball.py
@@ -75,11 +75,17 @@ class CountryballNamePrompt(Modal, title=f"Catch this {settings.collectible_name
             return
 
         if not self.view.is_name_valid(self.name.value):
+            if len(self.name.value) > 500:
+                wrong_name = self.name.value[:500] + "..."
+            else:
+                wrong_name = self.name.value
+
             wrong_message = random.choice(settings.wrong_messages).format(
                 user=interaction.user.mention,
                 collectible=settings.collectible_name,
                 ball=self.view.name,
                 collectibles=settings.plural_collectible_name,
+                wrong=wrong_name,
             )
 
             await interaction.followup.send(
