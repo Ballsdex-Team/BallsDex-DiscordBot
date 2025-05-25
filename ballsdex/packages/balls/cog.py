d6c484de4bcfc6bcd22dbe965ea7872d89f1f41b
AngerRandom <156929971+AngerRandom@users.noreply.github.com>
2025-05-25 19:27:09 +0300

    function change

--- ballsdex/packages/balls/cog.py
+++ ballsdex/packages/balls/cog.py
@@ -305,14 +305,14 @@ class Balls(commands.GroupCog, group_name=settings.players_group_cog_name):
             # check if we need to add "(continued)" to the field name
             first_field_added = False
             buffer = ""
-            brawler_emoji_1 = interaction.client.get_emoji(1371591594867949588)
-            brawler_emoji_2 = interaction.client.get_emoji(1371591872782401637)
-            brawler_emoji_3 = interaction.client.get_emoji(1371592384143556648)
-            brawler_emoji_4 = interaction.client.get_emoji(1371592712331198545)
-            unlock_emoji = interaction.client.get_emoji(1363692398596853841)
-            lock_emoji = interaction.client.get_emoji(1363692380208890066)
-            no_brawler_emoji = interaction.client.get_emoji(1349143586603667587)
-            completionist_emoji = interaction.client.get_emoji(1372373379939827744)
+            brawler_emoji_1 = self.bot.get_emoji(1371591594867949588)
+            brawler_emoji_2 = self.bot.get_emoji(1371591872782401637)
+            brawler_emoji_3 = self.bot.get_emoji(1371592384143556648)
+            brawler_emoji_4 = self.bot.get_emoji(1371592712331198545)
+            unlock_emoji = self.bot.get_emoji(1363692398596853841)
+            lock_emoji = self.bot.get_emoji(1363692380208890066)
+            no_brawler_emoji = self.bot.get_emoji(1349143586603667587)
+            completionist_emoji = self.bot.get_emoji(1372373379939827744)
 
             for emoji_id in emoji_ids:
                 emoji = self.bot.get_emoji(emoji_id)
