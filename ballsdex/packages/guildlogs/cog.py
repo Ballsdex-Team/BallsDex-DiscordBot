c90def34d77addae3b1baa9cd3344ea497353784
AngerRandom <156929971+AngerRandom@users.noreply.github.com>
2025-06-04 17:53:46 +0300

    .

--- ballsdex/packages/guildlogs/cog.py
+++ ballsdex/packages/guildlogs/cog.py
@@ -1,6 +1,10 @@
+import logging
+
 import discord
 from discord.ext import commands
 
 from ballsdex.core.bot import BallsDexBot
 from ballsdex.settings import Settings
-from ballsdex.packages.admin.cog import log_action
+from ballsdex.core.utils.logging import log_action
+
+log = logging.getLogger("ballsdex.packages.guildlogs")
