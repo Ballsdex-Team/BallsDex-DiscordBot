ea2be1348d1891a63225a767a32c97418de90c01
AngerRandom <156929971+AngerRandom@users.noreply.github.com>
2025-06-04 18:14:28 +0300

    Update cog.py

--- ballsdex/packages/guildlogs/cog.py
+++ ballsdex/packages/guildlogs/cog.py
@@ -5,7 +5,7 @@ import discord
 from discord.ext import commands
 
 from ballsdex.core.bot import BallsDexBot
-from ballsdex.settings import Settings
+from ballsdex.settings import settings
 from ballsdex.core.utils.logging import log_action
 
 log = logging.getLogger("ballsdex.packages.guildlogs")
