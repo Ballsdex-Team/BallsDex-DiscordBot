72bcdd36a5ff41e41861e6b6ce6d5e469a0c1e33
AngerRandom <156929971+AngerRandom@users.noreply.github.com>
2025-06-04 18:13:39 +0300

    final touches

--- ballsdex/packages/guildlogs/cog.py
+++ ballsdex/packages/guildlogs/cog.py
@@ -18,11 +18,18 @@ class GuildLogs(commands.Cog):
   async def on_guild_join(self, guild):
     join_time = datetime.now(timezone.utc)
     timestamp = f"<t:{int(join_time.timestamp())}:f>"
-    try:
-        await log_action(
-              f"BrawlDex joined {guild.name} {timestamp}. "
-              f"(ID: {guild.id})"
+    await log_action(
+              f"BrawlDex joined {guild.name} {timestamp}. ",
+              f"(ID: {guild.id})",
               self.bot,
             )
-    except Exception as e:
-         log.error(f"Failed to send the join log {e}")
+
+  @commands.Cog.listener()
+  async def on_guild_remove(self, guild):
+    leave_time = datetime.now(timezone.utc)
+    timestamp = f"<t:{int(leave_time.timestamp())}:f>"
+    await log_action(
+              f"BrawlDex left {guild.name} {timestamp}. ",
+              f"(ID: {guild.id})",
+              self.bot,
+            )
