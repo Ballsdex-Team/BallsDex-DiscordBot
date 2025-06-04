72ce659057401909996853b6310daa3661b2b365
AngerRandom <156929971+AngerRandom@users.noreply.github.com>
2025-06-04 18:03:27 +0300

    .

--- ballsdex/packages/guildlogs/cog.py
+++ ballsdex/packages/guildlogs/cog.py
@@ -1,4 +1,5 @@
 import logging
+from datetime import datetime, timezone
 
 import discord
 from discord.ext import commands
@@ -8,3 +9,15 @@ from ballsdex.settings import Settings
 from ballsdex.core.utils.logging import log_action
 
 log = logging.getLogger("ballsdex.packages.guildlogs")
+
+class GuildLogs(commands.Cog):
+  def __init__(self, bot: "BallsDexBot"):
+        self.bot = bot
+
+  @commands.Cog.listener()
+  async def on_guild_join(self, guild):
+    try:
+      await log_action(
+              f"BrawlDex joined 
+              self.bot,
+            )
