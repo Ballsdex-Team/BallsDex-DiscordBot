491acb2d316c3bf00cdd25e65efb21c41012e2d9
AngerRandom <156929971+AngerRandom@users.noreply.github.com>
2025-06-04 17:51:01 +0300

    Create cog.py

---
+++ ballsdex/packages/guildlogs/cog.py
@@ -0,0 +1,6 @@
+import discord
+from discord.ext import commands
+
+from ballsdex.core.bot import BallsDexBot
+from ballsdex.settings import Settings
+from ballsdex.packages.admin.cog import log_action
