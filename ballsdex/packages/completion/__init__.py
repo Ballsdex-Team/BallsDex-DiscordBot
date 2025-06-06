c92bdf9c4fe05442d24990fa412284b243ee896a
AngerRandom <156929971+AngerRandom@users.noreply.github.com>
2025-06-06 22:11:10 +0300

    .

--- ballsdex/packages/completion/__init__.py
+++ ballsdex/packages/completion/__init__.py
@@ -1,9 +1,9 @@
 from typing import TYPE_CHECKING
 
-from ballsdex.packages.completion.cog import Completion
+from ballsdex.packages.completion.cog import Collection
 
 if TYPE_CHECKING:
     from ballsdex.core.bot import BallsDexBot
 
 async def setup(bot: "BallsDexBot"):
-    await bot.add_cog(Completion(bot))
+    await bot.add_cog(Collection(bot))
