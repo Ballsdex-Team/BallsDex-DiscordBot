c4b308c2944a68910329bc3c0d239bff748d5079
AngerRandom <156929971+AngerRandom@users.noreply.github.com>
2025-06-08 13:07:28 +0300

    done

--- ballsdex/packages/joke/cog.py
+++ ballsdex/packages/joke/cog.py
@@ -32,4 +32,28 @@ class Joke(commands.Cog):
       """
       What Colgate toothpaste will you get today? Try your luck!
       """
-      toothpastes = [
+      toothpastes = ["mango", "strawberry", "mint", "chocolate", "amongus", "discord", "yippee", "brawl stars", "cement", "dynamike song tutorial"]
+      picked_toothpaste = random.choice(toothpastes)
+      await ctx.send(f"colgate {picked_toothpaste} toothpaste")
+
+  @colgate.error
+  async def colgate_error(self, ctx, error):
+        if isinstance(error, commands.CommandOnCooldown):
+            return
+
+  @commands.command()
+  @commands.cooldown(1, 60, commands.BucketType.user)
+  async def jfbd(self, ctx: commands.Context):
+      """
+      Justice for BrawlDex!
+      """
+      emoji_1 = ctx.bot.get_emoji(1364595411213619200)
+      emoji_2 = ctx.bot.get_emoji(1368258491340427396)
+      messages = ["#justiceforbrawldex", "Justice for BrawlDex!", emoji_1, emoji_2]
+      picked_message = random.choice(messages)
+      await ctx.send(picked_message)
+
+      @jfbd.error
+      async def jfbd_error(self, ctx, error):
+            if isinstance(error, commands.CommandOnCooldown):
+                return
