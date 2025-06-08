8861a7deba30ec75aa72f8d4d26f93f92036954c
AngerRandom <156929971+AngerRandom@users.noreply.github.com>
2025-06-08 12:35:25 +0300

    first things for the cog

---
+++ ballsdex/packages/joke/cog.py
@@ -0,0 +1,11 @@
+import random
+import logging
+import asyncio
+
+import discord
+from discord.ext import commands
+
+from ballsdex.settings import settings
+from ballsdex.core.bot import BallsDexBot
+
+log = logging.getLogger("ballsdex.packages.joke")
