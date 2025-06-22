e7b11cae249ff8e3d76b22e70184899d38731b51
AngerRandom <156929971+AngerRandom@users.noreply.github.com>
2025-06-22 20:42:02 +0300

    reset

--- ballsdex/core/image_generator/image_gen.py
+++ ballsdex/core/image_generator/image_gen.py
@@ -10,6 +10,7 @@ from ballsdex.settings import settings
 if TYPE_CHECKING:
     from ballsdex.core.models import BallInstance
 
+
 SOURCES_PATH = Path(os.path.dirname(os.path.abspath(__file__)), "./src")
 WIDTH = 1500
 HEIGHT = 2000
@@ -17,7 +18,7 @@ HEIGHT = 2000
 RECTANGLE_WIDTH = WIDTH - 40
 RECTANGLE_HEIGHT = (HEIGHT // 5) * 2
 
-CORNERS = ((35, 261), (1395, 991))
+CORNERS = ((34, 261), (1393, 992))
 artwork_size = [b - a for a, b in zip(*CORNERS)]
 
 # ===== TIP =====
@@ -32,47 +33,30 @@ artwork_size = [b - a for a, b in zip(*CORNERS)]
 # image viewer. There are options available to specify the ball or the special background,
 # use the "--help" flag to view all options.
 
-title_font = ImageFont.truetype(str(SOURCES_PATH / "LilitaOne-Regular.ttf"), 170)
-capacity_name_font = ImageFont.truetype(str(SOURCES_PATH / "LilitaOne-Regular.ttf"), 110)
-capacity_description_font = ImageFont.truetype(str(SOURCES_PATH / "LilitaOne-Regular.ttf"), 75)
-stats_font = ImageFont.truetype(str(SOURCES_PATH / "LilitaOne-Regular.ttf"), 130)
+title_font = ImageFont.truetype(str(SOURCES_PATH / "ArsenicaTrial-Extrabold.ttf"), 170)
+capacity_name_font = ImageFont.truetype(str(SOURCES_PATH / "Bobby Jones Soft.otf"), 110)
+capacity_description_font = ImageFont.truetype(str(SOURCES_PATH / "OpenSans-Semibold.ttf"), 75)
+stats_font = ImageFont.truetype(str(SOURCES_PATH / "Bobby Jones Soft.otf"), 130)
 credits_font = ImageFont.truetype(str(SOURCES_PATH / "arial.ttf"), 40)
 
 credits_color_cache = {}
 
+
 def get_credit_color(image: Image.Image, region: tuple) -> tuple:
     image = image.crop(region)
-    brightness = sum(image.convert("L").getdata()) / image.width / image.height
-    return (255, 255, 255, 255) if brightness > 100 else (255, 255, 255, 255)
+    brightness = sum(image.convert("L").getdata()) / image.width / image.height  # type: ignore
+    return (0, 0, 0, 255) if brightness > 100 else (255, 255, 255, 255)
 
-def wrap_text(text: str, font: ImageFont.FreeTypeFont, max_width: int, draw: ImageDraw.ImageDraw) -> list[str]:
-    paragraphs = text.split('%%')
-    lines = []
-    for para in paragraphs:
-        words = para.strip().split(' ')
-        current_line = ''
-        for word in words:
-            test_line = f"{current_line} {word}".strip()
-            if draw.textlength(test_line, font=font) <= max_width:
-                current_line = test_line
-            else:
-                if current_line:
-                    lines.append(current_line)
-                current_line = word
-        if current_line:
-            lines.append(current_line)
-    return lines
 
 def draw_card(
     ball_instance: "BallInstance",
     media_path: str = "./admin_panel/media/",
 ) -> tuple[Image.Image, dict[str, Any]]:
     ball = ball_instance.countryball
-    ball_health = (86, 255, 100, 255)
+    ball_health = (237, 115, 101, 255)
     ball_credits = ball.credits
     special_credits = ""
     card_name = ball.cached_regime.name
-
     if special_image := ball_instance.special_card:
         card_name = getattr(ball_instance.specialcard, "name", card_name)
         image = Image.open(media_path + special_image)
@@ -80,7 +64,6 @@ def draw_card(
             special_credits += f" • Special Author: {ball_instance.specialcard.credits}"
     else:
         image = Image.open(media_path + ball.cached_regime.background)
-
     image = image.convert("RGBA")
     icon = (
         Image.open(media_path + ball.cached_economy.icon).convert("RGBA")
@@ -89,97 +72,59 @@ def draw_card(
     )
 
     draw = ImageDraw.Draw(image)
-    shadow_color = "black"
-    shadow_offset = 3
-    
-    draw.text(
-        (50, 20 + shadow_offset),
-        ball.short_name or ball.country,
-        font=title_font,
-        fill=shadow_color,
-        stroke_width=8,
-        stroke_fill=(0, 0, 0, 255),
-    )
-
-    # Title
     draw.text(
         (50, 20),
         ball.short_name or ball.country,
         font=title_font,
-        fill=(255, 255, 255, 255),
-        stroke_width=8,
-       stroke_fill=(0, 0, 0, 255),
+        stroke_width=2,
+        stroke_fill=(0, 0, 0, 255),
     )
 
-    # Capacity Name
-    cap_name = textwrap.wrap(f"{ball.capacity_name}", width=26)
+    cap_name = textwrap.wrap(f"Ability: {ball.capacity_name}", width=26)
+
     for i, line in enumerate(cap_name):
         draw.text(
             (100, 1050 + 100 * i),
             line,
             font=capacity_name_font,
-            fill=(255, 255, 255, 255),
-            stroke_width=3,
+            fill=(230, 230, 230, 255),
+            stroke_width=2,
             stroke_fill=(0, 0, 0, 255),
         )
-
-    # Capacity Description with custom line breaks (%%)
-    max_text_width = 1320
-    wrapped_description = wrap_text(ball.capacity_description, capacity_description_font, max_text_width, draw)
-    for i, line in enumerate(wrapped_description):
+    for i, line in enumerate(textwrap.wrap(ball.capacity_description, width=32)):
         draw.text(
-            (60, 1100 + 100 * len(cap_name) + 80 * i + shadow_offset),
+            (60, 1100 + 100 * len(cap_name) + 80 * i),
             line,
             font=capacity_description_font,
-            fill=shadow_color,
-            stroke_width=5,
+            stroke_width=1,
             stroke_fill=(0, 0, 0, 255),
         )
 
-    # Rarity display
-    if settings.show_rarity:
-        draw.text(
-            (60, 1160 + 80 * i),
-            line,
-            font=capacity_description_font,
-            stroke_width=5,
-            stroke_fill=(0, 0, 0, 255),
-        )
-
-    draw.text(
-        (320, 1670 + shadow_offset),
-        str(ball_instance.health),
-        font=stats_font,
-        fill=shadow_color,
-        stroke_width=5,
-        stroke_fill=(0, 0, 0, 255),
-    )
     draw.text(
         (320, 1670),
         str(ball_instance.health),
         font=stats_font,
         fill=ball_health,
-        stroke_width=5,
+        stroke_width=1,
         stroke_fill=(0, 0, 0, 255),
-    )
-    draw.text(
-        (1120, 1670 + shadow_offset),
-        str(ball_instance.attack),
-        font=stats_font,
-        fill=shadow_color,
-        stroke_width=5,
-        stroke_fill=(0, 0, 0, 255),
-        anchor="ra",
     )
     draw.text(
         (1120, 1670),
         str(ball_instance.attack),
         font=stats_font,
-        fill=(255, 66, 92, 255),
-        stroke_width=5,
+        fill=(252, 194, 76, 255),
+        stroke_width=1,
         stroke_fill=(0, 0, 0, 255),
         anchor="ra",
     )
+    if settings.show_rarity:
+        draw.text(
+            (1200, 50),
+            str(ball.rarity),
+            font=stats_font,
+            stroke_width=2,
+            stroke_fill=(0, 0, 0, 255),
+        )
     if card_name in credits_color_cache:
         credits_color = credits_color_cache[card_name]
     else:
@@ -191,22 +136,20 @@ def draw_card(
         (30, 1870),
         # Modifying the line below is breaking the licence as you are removing credits
         # If you don't want to receive a DMCA, just don't
-        f"Ballsdex by El Laggron, BrawlDex by AngerRandom, Brawl Stars by Supercell\n" f"{ball_credits}",
-       font=credits_font,
+        f"Created by El Laggron{special_credits}\n" f"Artwork author: {ball_credits}",
+        font=credits_font,
         fill=credits_color,
-        stroke_width=3,
-        stroke_fill=(0, 0, 0, 255),
+        stroke_width=0,
+        stroke_fill=(255, 255, 255, 255),
     )
 
-    # Artwork
     artwork = Image.open(media_path + ball.collection_card).convert("RGBA")
-    image.paste(ImageOps.fit(artwork, artwork_size), CORNERS[0])
+    image.paste(ImageOps.fit(artwork, artwork_size), CORNERS[0])  # type: ignore
 
-    # Icon
     if icon:
         icon = ImageOps.fit(icon, (192, 192))
         image.paste(icon, (1200, 30), mask=icon)
         icon.close()
     artwork.close()
 
-    return image, {"format": "PNG"}
+    return image, {"format": "WEBP"}
