63bd76e684b19a6395613df7f28c3402fea56a44
AngerRandom <156929971+AngerRandom@users.noreply.github.com>
2025-06-26 16:51:12 +0300

    .

--- ballsdex/packages/automod.py
+++ ballsdex/packages/automod.py
@@ -9,3 +9,53 @@ from ballsdex.core.bot import BallsDexBot
 
 log = logging.getLogger("ballsdex.packages.automod")
 
+class AutoMod(commands.Cog):
+  def __init__(self, bot: "BallsDexBot"):
+        self.bot = bot
+
+  @commands.Cog.listener()
+  async def on_reaction_add(reaction, user):
+    BANNED_REACTIONS = [
+      🫃,
+      🫄,
+      🤰,
+      🤱, 
+      🧑‍🍼,
+      👩‍🍼,
+      👨‍🍼, 
+      🧑‍🦽, 
+      👩‍🦽,
+      👨‍🦽,
+      🧑‍🦼,
+      👩‍🦼,
+      👨‍🦼,
+      🧑‍🦯,
+      👩‍🦯,
+      👨‍🦯,
+      🐕‍🦺,
+      🪚,
+      ♿
+    ]
+      
+    if reaction.message.guild is None:
+        return
+    if reaction.message.guild.id not in settings.admin_guild_ids:
+        return
+    if user.bot:
+        return
+    if isinstance(user, discord.Member):
+        user_role_ids = {role.id for role in user.roles}
+        for role in user_role_ids:
+          if role in settings.root_role_ids:
+            return
+    if str(reaction.emoji) in BANNED_REACTIONS:
+      try:
+          await reaction.remove(user)
+          await user.timeout(timedelta(seconds=60), reason="Reacting with banned emojis")
+      except Exception:
+          log.exception("An error occurred with Reaction AutoMod.", exc_info=True)
+
+async def setup(bot: "BallsDexBot"):
+    await bot.add_cog(AutoMod(bot))
+          
+    
