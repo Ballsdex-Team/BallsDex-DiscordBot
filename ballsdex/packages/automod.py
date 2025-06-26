4767a97600648ec71bd261b368c387365735a945
AngerRandom <156929971+AngerRandom@users.noreply.github.com>
2025-06-26 16:29:25 +0300

    d

---
+++ ballsdex/packages/automod.py
@@ -0,0 +1,11 @@
+import logging
+from datetime import datetime, timedelta
+
+import discord
+from discord.ext import commands
+
+from ballsdex.settings import settings
+from ballsdex.core.bot import BallsDexBot
+
+log = logging.getLogger("ballsdex.packages.automod")
+
