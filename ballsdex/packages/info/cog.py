f2bf22e95b2d131a4379754331238ef00befe66b
AngerRandom <156929971+AngerRandom@users.noreply.github.com>
2025-06-29 22:05:48 +0300

    syntax fix attempt #6

--- ballsdex/packages/info/cog.py
+++ ballsdex/packages/info/cog.py
@@ -33,9 +33,9 @@ sections = [
 ]
 
 class SectionPaginator(discord.ui.View):
-    def __init__(self, bot: "BallsDexBot", sections: list[dict], author: discord.User):
+    def __init__(self, sections: list[dict], author: discord.User):
         super().__init__(timeout=300)
-        self.bot = bot
+        self.bot = "BallsDexBot"
         self.sections = sections
         self.author = author
         self.message = None
