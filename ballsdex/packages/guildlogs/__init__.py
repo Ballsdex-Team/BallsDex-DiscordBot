f791bd5d30af8b873fc0d49d4bfffb5750f63566
AngerRandom <156929971+AngerRandom@users.noreply.github.com>
2025-07-01 15:35:02 +0300

    adding bot status log

--- ballsdex/packages/guildlogs/__init__.py
+++ ballsdex/packages/guildlogs/__init__.py
@@ -1,4 +1,7 @@
 from typing import TYPE_CHECKING
+from datetime import datetime
+
+import discord
 
 from ballsdex.packages.guildlogs.cog import GuildLogs
 
@@ -7,4 +10,20 @@ if TYPE_CHECKING:
 
 
 async def setup(bot: "BallsDexBot"):
+    channel = bot.get_channel(1295411468765888592)
+    embed = discord.Embed(
+        description=f":green_circle: {bot.user.mention} is now online!",
+        color=discord.Colour.green(),
+        timestamp=datetime.utcnow()
+    )
     await bot.add_cog(GuildLogs(bot))
+    await channel.send(embed=embed)
+
+async def teardown(bot: "BallsDexBot"):
+    channel = bot.get_channel(1295411468765888592)
+    embed = discord.Embed(
+        description=f":red_circle: {bot.user.mention} is now offline!",
+        color=discord.Colour.red(),
+        timestamp=datetime.utcnow()
+    )
+    await channel.send(embed=embed)
