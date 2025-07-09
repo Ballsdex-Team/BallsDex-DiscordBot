8219d1592d5c54d4a32c3b6a34a898af0280de00
AngerRandom <156929971+AngerRandom@users.noreply.github.com>
2025-07-09 17:05:11 +0300

    Update poll.py

--- ballsdex/packages/poll.py
+++ ballsdex/packages/poll.py
@@ -9,3 +9,10 @@ from discord.ui import View, Button, button
 from ballsdex.core.models import Ball
 from ballsdex.core.utils.transformers import BallEnabledTransform
 from ballsdex.settings import settings
+
+if TYPE_CHECKING:
+  from ballsdex.core.bot import BallsDexBot
+
+log = logging.getLogger("ballsdex.packages.poll")
+
+class PollCreatorView(View):
