e47469b82e05ef5dc0f771f3ab29cfe0dbe5ba76
AngerRandom <156929971+AngerRandom@users.noreply.github.com>
2025-07-10 14:31:07 +0300

    Update and rename __init__.py to __init__.py

--- ballsdex/packages/staff/__init__.py
+++ ballsdex/packages/staff/__init__.py
@@ -1,9 +1,9 @@
 from typing import TYPE_CHECKING
 
-from ballsdex.packages.cardmaker.cog import CardMaker
+from ballsdex.packages.staff.cog import Staff
 
 if TYPE_CHECKING:
     from ballsdex.core.bot import BallsDexBot
 
 async def setup(bot: "BallsDexBot"):
-    await bot.add_cog(CardMaker(bot))
+    await bot.add_cog(Staff(bot))
