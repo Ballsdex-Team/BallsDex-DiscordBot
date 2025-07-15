63b494e21251097c45961e6fa64a7db526c0cbf9
AngerRandom <156929971+AngerRandom@users.noreply.github.com>
2025-07-15 20:48:09 +0300

    Update poll.py

--- ballsdex/packages/poll.py
+++ ballsdex/packages/poll.py
@@ -16,6 +16,12 @@ if TYPE_CHECKING:
 
 log = logging.getLogger("ballsdex.packages.poll")
 
+def list_validator(multi_string):
+    items = multi_string.split("|")
+    if any(item.strip() == "" for item in items):
+        raise ValueError("One or more items are empty or contain only whitespace.")
+    return items
+
 class Poll(commands.GroupCog):
   """
   Poll builder command.
@@ -30,17 +36,26 @@ class Poll(commands.GroupCog):
         app_commands.Choice(name="Collectible", value="collectible"),
         app_commands.Choice(name="Custom", value="custom")
         ])
-  @app_commands.describe(type="Type of the poll")
+  @app_commands.describe(poll_type="Type of the poll")
   @app_commands.describe(duration="Duration of the poll (hours)")
   @app_commands.describe(allow_multiple="Whether to allow multiple select or not")
-  @app_commands.describe(answers="Answers of the poll (Max 10)")
+  @app_commands.describe(answers="Answers of the poll (Max 10). Each answer seperated with a colon (|).")
   @app_commands.checks.cooldown(1, 3, key=lambda i: i.user.id)
   async def create(
     self,
     interaction: discord.Interaction["BallsDexBot"],
     question: str,
-    type: app_commands.Choice[str],
+    poll_type: app_commands.Choice[str],
     duration: int,
     allow_multiple: bool,
     answers: str
   ):
+    try:
+        formatted_array = list(list_validator(answers))
+    except ValueError:
+        await interaction.response.send_message("The answers format you provided is invalid. Please try again.", ephemeral=True)
+    if len(formatted_array) > 10:
+        await interaction.response.send_message("You can't have more than 10 answers.", ephemeral=True)
+    if poll_type == "collectible":
+      
+      
