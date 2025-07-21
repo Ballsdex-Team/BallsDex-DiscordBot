0d0944624bf1e95ad79a03b821378d081f62165d
dormieriancitizen <dormieriancitizen@gmail.com>
2025-07-21 14:05:02 -0400

    Make default template a json, fix the schema, move things about

--- ballsdex/core/image_generator/image_gen.py
+++ ballsdex/core/image_generator/image_gen.py
@@ -1,13 +1,12 @@
 from __future__ import annotations
 
+import json
 import os
 import textwrap
 from pathlib import Path
 from typing import TYPE_CHECKING, Any, NamedTuple
 
 from PIL import Image, ImageDraw, ImageFont, ImageOps
-
-from ballsdex.core.image_generator.default_card_template import DEFAULT_CARD_TEMPLATE
 
 if TYPE_CHECKING:
     from PIL.ImageFont import FreeTypeFont
@@ -31,6 +30,9 @@ SOURCES_PATH = Path(os.path.dirname(os.path.abspath(__file__)), "./src")
 text_color_cache: dict[Regime | Special, dict["str", tuple[int, int, int, int]]] = {}
 font_cache: dict[str, FreeTypeFont] = {}
 
+with Path(__file__).with_name("default_card_template.json").open("r") as f:
+    default_card_template = json.load(f)
+
 
 def draw_card(
     ball_instance: "BallInstance",
@@ -41,7 +43,7 @@ def draw_card(
     if ball_instance.card_template:
         template = ball_instance.card_template.template
     if not template:
-        template = DEFAULT_CARD_TEMPLATE
+        template = default_card_template
 
     template = CardTemplate(**template)  # type: ignore
 
@@ -68,7 +70,9 @@ class TemplateLayer(NamedTuple):
     is_image: bool
     source: list[str] | str
     anchor: list[int | str]
+
     size: tuple[int, int] = (0, 0)
+    offset: tuple[int, int] = (0, 0)
 
     # Template string with $data to-be-replaced by the data
     text_template: str | None = None
@@ -86,21 +90,6 @@ class LayerInfo(NamedTuple):
     finished_coords: tuple[int, int]
 
 
-def get_attribute_recursive(object: Any, attribute: str) -> Any:
-    data = object
-    for subattr in attribute.split("."):
-        data = getattr(data, subattr, None)
-        if data is None:
-            return None
-    return data
-
-
-def get_region_best_color(image: Image.Image, region: tuple) -> tuple:
-    image = image.crop(region)
-    brightness = sum(image.convert("L").getdata()) / image.width / image.height  # type: ignore
-    return (0, 0, 0, 255) if brightness > 100 else (255, 255, 255, 255)
-
-
 def draw_layer(
     image: Image.Image,
     layer: TemplateLayer,
@@ -108,92 +97,6 @@ def draw_layer(
     media_path: str,
     prior_layer_info: dict[str, LayerInfo],
 ):
-    def get_font(layer: TemplateLayer) -> FreeTypeFont:
-        if layer.text_font in font_cache:
-            font = font_cache[layer.text_font]
-        else:
-            font = ImageFont.truetype(str(SOURCES_PATH / layer.text_font), layer.text_font_size)
-            font_cache[layer.text_font] = font
-
-        return font
-
-    def get_text_fill_color(
-        layer: TemplateLayer, ball_instance: BallInstance, region: tuple[int, int, int, int]
-    ) -> tuple[int, int, int, int]:
-        text_fill: tuple[int, int, int, int] | None = None
-        if layer.text_fill == "auto":
-            ball_bg_cache = (
-                ball_instance.special
-                if ball_instance.special
-                else ball_instance.countryball.regime
-            )
-            if ball_bg_cache in text_color_cache:
-                if layer.name in text_color_cache[ball_bg_cache]:
-                    text_fill = text_color_cache[ball_bg_cache][layer.name]
-
-            if not text_fill:
-                text_fill = get_region_best_color(image, region)
-        if not text_fill:
-            if isinstance(layer.text_fill, str):
-                text_fill = (255, 255, 255, 255)
-            else:
-                text_fill = tuple(layer.text_fill)  # pyright: ignore[reportAssignmentType]
-
-        return text_fill  # pyright: ignore[reportReturnType]
-
-    def get_text_strings(layer: TemplateLayer, data) -> list[str]:
-        final_str: str = (
-            layer.text_template.replace("$data", data) if layer.text_template else data
-        )
-
-        final_strs: list[str]
-        if layer.text_wrap:
-            final_strs = textwrap.wrap(final_str, width=layer.text_wrap, expand_tabs=True)
-        else:
-            final_strs = [final_str]
-
-        return final_strs
-
-    def get_anchor_coords(layer, prior_layer_info) -> tuple[int, int]:
-        if isinstance(layer.anchor[0], int):
-            startx = layer.anchor[0]
-        else:
-            # Essentially putting in a string means, start this layer
-            # where that other layer finished
-            startx = prior_layer_info[layer.anchor[0]].finished_coords[0]
-
-        if isinstance(layer.anchor[1], int):
-            starty = layer.anchor[1]
-        else:
-            starty = prior_layer_info[layer.anchor[1]].finished_coords[1]
-
-        return (startx, starty)
-
-    def get_layer_data(layer: TemplateLayer, ball_instance: BallInstance) -> str | None:
-        data: str | None = None
-
-        if not layer.is_attribute:
-            if isinstance(layer.source, list):
-                data = layer.source[0]
-            else:
-                data = layer.source
-
-            return str(data)
-
-        if isinstance(layer.source, list):
-            # for prioritised source lists, eg special > regime background
-            for attribute in layer.source:
-                data = get_attribute_recursive(ball_instance, attribute)
-                if data:
-                    break
-        else:
-            data = get_attribute_recursive(ball_instance, layer.source)
-
-        if data:
-            data = str(data)
-
-        return data
-
     name = layer.name
     start_coords = get_anchor_coords(layer, prior_layer_info)
 
@@ -218,9 +121,9 @@ def draw_layer(
         text_width = max(draw.textlength(text=string, font=font) for string in final_strs)
         end_coords = (
             int(start_coords[0] + text_width),
-            start_coords[1] + layer.text_line_height * (len(final_strs)),
+            start_coords[1] + layer.text_line_height * len(final_strs),
         )
-        text_fill = get_text_fill_color(layer, ball_instance, (*start_coords, *end_coords))
+        text_fill = get_text_fill_color(layer, ball_instance, (*start_coords, *end_coords), image)
 
         for i, line in enumerate(final_strs):
             draw.text(
@@ -228,9 +131,111 @@ def draw_layer(
                 line,
                 font=font,
                 fill=text_fill,
-                stroke_width=2,
+                stroke_width=layer.text_stroke_width,
                 stroke_fill=(0, 0, 0, 255),
                 anchor=layer.text_anchor,
             )
 
     prior_layer_info[name] = LayerInfo(finished_coords=end_coords)
+
+
+def get_attribute_recursive(object: Any, attribute: str) -> Any:
+    data = object
+    for subattr in attribute.split("."):
+        data = getattr(data, subattr, None)
+        if data is None:
+            return None
+    return data
+
+
+def get_region_best_color(image: Image.Image, region: tuple[int, int, int, int]) -> tuple:
+    image = image.crop(region)
+    brightness = sum(image.convert("L").getdata()) / image.width / image.height  # type: ignore
+    return (0, 0, 0, 255) if brightness > 100 else (255, 255, 255, 255)
+
+
+def get_font(layer: TemplateLayer) -> FreeTypeFont:
+    if layer.text_font in font_cache:
+        font = font_cache[layer.text_font]
+    else:
+        font = ImageFont.truetype(str(SOURCES_PATH / layer.text_font), layer.text_font_size)
+        font_cache[layer.text_font] = font
+
+    return font
+
+
+def get_text_fill_color(
+    layer: TemplateLayer, ball_instance: BallInstance, region: tuple[int, int, int, int], image
+) -> tuple[int, int, int, int]:
+    text_fill: tuple[int, int, int, int] | None = None
+    if layer.text_fill == "auto":
+        ball_bg_cache = (
+            ball_instance.special if ball_instance.special else ball_instance.countryball.regime
+        )
+        if ball_bg_cache in text_color_cache:
+            if layer.name in text_color_cache[ball_bg_cache]:
+                text_fill = text_color_cache[ball_bg_cache][layer.name]
+
+        if not text_fill:
+            text_fill = get_region_best_color(image, region)
+    if not text_fill:
+        if isinstance(layer.text_fill, str):
+            text_fill = (255, 255, 255, 255)
+        else:
+            text_fill = tuple(layer.text_fill)  # pyright: ignore[reportAssignmentType]
+
+    return text_fill  # pyright: ignore[reportReturnType]
+
+
+def get_text_strings(layer: TemplateLayer, data) -> list[str]:
+    final_str: str = layer.text_template.replace("$data", data) if layer.text_template else data
+
+    final_strs: list[str]
+    if layer.text_wrap:
+        final_strs = textwrap.wrap(final_str, width=layer.text_wrap, expand_tabs=True)
+    else:
+        final_strs = [final_str]
+
+    return final_strs
+
+
+def get_anchor_coords(layer: TemplateLayer, prior_layer_info) -> tuple[int, int]:
+    if isinstance(layer.anchor[0], int):
+        startx = layer.anchor[0]
+    else:
+        # Essentially putting in a string means, start this layer
+        # where that other layer finished
+        startx = prior_layer_info[layer.anchor[0]].finished_coords[0]
+
+    if isinstance(layer.anchor[1], int):
+        starty = layer.anchor[1]
+    else:
+        starty = prior_layer_info[layer.anchor[1]].finished_coords[1]
+
+    return (startx + layer.offset[0], starty + layer.offset[1])
+
+
+def get_layer_data(layer: TemplateLayer, ball_instance: BallInstance) -> str | None:
+    data: str | None = None
+
+    if not layer.is_attribute:
+        if isinstance(layer.source, list):
+            data = layer.source[0]
+        else:
+            data = layer.source
+
+        return str(data)
+
+    if isinstance(layer.source, list):
+        # for prioritised source lists, eg special > regime background
+        for attribute in layer.source:
+            data = get_attribute_recursive(ball_instance, attribute)
+            if data:
+                break
+    else:
+        data = get_attribute_recursive(ball_instance, layer.source)
+
+    if data:
+        data = str(data)
+
+    return data
