30763442a74ea80a85780f69fedeb6357b36b286
AngerRandom <156929971+AngerRandom@users.noreply.github.com>
2025-07-28 20:09:19 +0300

    Create dailycaughtreset.py

---
+++ ballsdex/packages/countryballs/dailycaughtreset.py
@@ -0,0 +1,22 @@
+import logging
+import asyncio
+import discord
+from apscheduler.schedulers.blocking import BlockingScheduler
+from ballsdex.core.models import Player
+from ballsdex.core.bot import BallsDexBot
+from ballsdex.core.utils.logging import log_action
+
+log = logging.getLogger("ballsdex.packages.countryballs")
+
+async def dailycaughtreset(self, bot: BallsDexBot):
+    top_players = await Player.all().order_by("-dailycaught").limit(30)
+    lines = []
+    for i, player in enumerate(top_players, 1):
+        lines.append(f"{i}. {player.discord_id} —  {player.dailycaught}")
+    lb_text = "\n".join(lines)
+    try:
+        await log_action(f"Daily caught has been reset.\n{lb_text}", self.bot)
+        await Player.filter(dailycaught__gt=0).update(dailycaught=0)
+    except Exception as e:
+        log.error("An error occured while triggering the daily caught reset", exc_info=e)
+    
