a5373d9c946c2d895d86525c3ef218b3e0d2ae6d
flaree <flare2399@gmail.com>
2025-12-12 20:54:44 +0000

    formatting

--- ballsdex/packages/balls/countryballs_paginator.py
+++ ballsdex/packages/balls/countryballs_paginator.py
@@ -146,9 +146,11 @@ class DuplicateViewMenu(Pages):
             )
         else:
             countryball = await Ball.get(country=item.values[0])
-            balls_query = balls_query.filter(ball=countryball).annotate(
-                specials=RawSQL("SUM(CASE WHEN special_id IS NULL THEN 0 ELSE 1 END)")
-            ).exclude(special__hidden=True)
+            balls_query = (
+                balls_query.filter(ball=countryball)
+                .annotate(specials=RawSQL("SUM(CASE WHEN special_id IS NULL THEN 0 ELSE 1 END)"))
+                .exclude(special__hidden=True)
+            )
             grouped_query = (
                 BallInstance.filter(player=player, ball=countryball)
                 .exclude(special=None)
