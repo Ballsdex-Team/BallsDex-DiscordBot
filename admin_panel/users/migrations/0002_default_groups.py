Ballsdex-Team/BallsDex-DiscordBot

commit 2277755c5671abf4bb5c3ee6d77e7d86157ce2e7

users: fix permissions name not being filled on new instances
Fixes #766

admin_panel/users/migrations/0002_default_groups.py
@@ -2,6 +2,7 @@
 
 from typing import TYPE_CHECKING, Literal, cast
 
+from django.contrib.auth.management import create_permissions
 from django.db import migrations
 
 if TYPE_CHECKING:
@@ -27,14 +28,14 @@ def get_permissions(
             perms = ["add", "change", "delete", "view"]
         for perm in perms:
             result.append(
-                permission_model.objects.get_or_create(
-                    content_type=content_type, codename=f"{perm}_{model._meta.model_name}"
-                )[0]
+                permission_model.objects.get(content_type=content_type, codename=f"{perm}_{model._meta.model_name}")
             )
     return result
 
 
 def create_default_groups(apps: "Apps", schema_editor: "BaseDatabaseSchemaEditor"):
+    create_permissions(app_config=apps.get_app_config("bd_models"), apps=apps, using=schema_editor.connection.alias)
+
     group_model = cast(type["Group"], apps.get_model("auth", "Group"))
     permission_model = cast(type["Permission"], apps.get_model("auth", "Permission"))
     content_type_model = cast(type["ContentType"], apps.get_model("contenttypes", "ContentType"))

commit 39da01079fd4a207fa69fbe3f169a3304622d2a4

Fix bug introduced in 2277755c5671abf4bb5c3ee6d77e7d86157ce2e7

admin_panel/users/migrations/0002_default_groups.py
@@ -2,6 +2,7 @@
 
 from typing import TYPE_CHECKING, Literal, cast
 
+from django.apps import apps as global_apps
 from django.contrib.auth.management import create_permissions
 from django.db import migrations
 
@@ -34,7 +35,9 @@ def get_permissions(
 
 
 def create_default_groups(apps: "Apps", schema_editor: "BaseDatabaseSchemaEditor"):
-    create_permissions(app_config=apps.get_app_config("bd_models"), apps=apps, using=schema_editor.connection.alias)
+    create_permissions(
+        app_config=global_apps.get_app_config("bd_models"), apps=apps, using=schema_editor.connection.alias
+    )
 
     group_model = cast(type["Group"], apps.get_model("auth", "Group"))
     permission_model = cast(type["Permission"], apps.get_model("auth", "Permission"))