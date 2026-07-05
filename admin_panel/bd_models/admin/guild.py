41d9f65c56445480ef634949955f8adb7812b606
Jamie <31554168+flaree@users.noreply.github.com>
2026-07-05 10:53:04 +0100

    [panel] guild ball sorting + add catch date and lock status to ball instance tab (#809)
    * Panel fixes for guild sorting and add locked status to ball tab
    
    * reformat

--- admin_panel/bd_models/admin/guild.py
+++ admin_panel/bd_models/admin/guild.py
@@ -33,13 +33,24 @@ class BallInstanceGuildTabular(InlinePaginated, NonrelatedInlineMixin, admin.Tab
     can_delete = False
 
     def get_form_queryset(self, obj: GuildConfig):
-        return BallInstance.objects.filter(server_id=obj.guild_id).prefetch_related("player", "ball", "special")
+        return (
+            BallInstance.objects.filter(server_id=obj.guild_id)
+            .prefetch_related("player", "ball", "special")
+            .order_by("-catch_date")
+        )
 
     @admin.display(description="Time to catch")
     def catch_time(self, obj: BallInstance):
         if obj.spawned_time:
             return str(obj.spawned_time - obj.catch_date)
         return "-"
+
+    @admin.display(description="Countryball")
+    def description(self, obj: BallInstance):
+        text = obj.description()
+        if obj.trade_player_id:
+            text += " 🔄"
+        return text
 
     # adding a countryball cannot work from here since all fields are readonly
     def has_add_permission(  # pyright: ignore [reportIncompatibleMethodOverride]
