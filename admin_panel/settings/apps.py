2866d0af368ad564e67c225eecc5a768b6443b29
El Laggron <auguste.charpentier@datadoghq.com>
2026-08-07 16:51:42 +0200

    Allow Django to start without a DB
    Many management commands don't need a db (migration generation, checks...) but the startup settings load caused a crash anyway. This allows these commands to run.

--- admin_panel/settings/apps.py
+++ admin_panel/settings/apps.py
@@ -1,6 +1,5 @@
 import asyncio
 import logging
-import sys
 
 from asgiref.sync import sync_to_async
 from django.apps import AppConfig
@@ -13,15 +12,6 @@ class SettingsConfig(AppConfig):
     name = "settings"
 
     def ready(self):
-        # TODO: change this mess
-        if (
-            "makemigrations" in sys.argv
-            or "migrate" in sys.argv
-            or "startapp" in sys.argv
-            or "collectstatic" in sys.argv
-        ):
-            return
-
         from .models import load_settings
 
         try:
@@ -31,4 +21,9 @@ class SettingsConfig(AppConfig):
             task.add_done_callback(lambda t: log.info("Settings read successfully."))
         except RuntimeError:
             # if the bot is running in a sync context
-            load_settings()
+            try:
+                load_settings()
+            except Exception:
+                # database isn't available
+                # we should still allow Django to keep starting, many management commands don't need settings
+                log.warning("Settings cannot be loaded.")
